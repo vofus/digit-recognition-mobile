bec0f3cddcc806e0ac5d8c7ce8180a7d546de87b
Andrey Vasin <vofuss@gmail.com>
2018-11-21 18:41:46 +0400

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(home: HomePage()));
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List<Offset> _points = <Offset>[];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: GestureDetector(
+          onPanUpdate: (DragUpdateDetails details) {
+            setState(() {
+              RenderBox obj = context.findRenderObject();
+              Offset _localPosition = obj.globalToLocal(details.globalPosition);
+              _points = List.from(_points)..add(_localPosition);
+            });
+          },
+          onPanEnd: (DragEndDetails details) => _points.add(null),
+          child: CustomPaint(
+            painter: Signature(points: _points),
+            size: Size.infinite,
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: new Icon(Icons.clear),
+        onPressed: () => _points.clear(),
+      ),
+    );
+  }
+}
+
+class Signature extends CustomPainter {
+  List<Offset> points;
+
+  Paint _paint = new Paint()
+    ..color = Colors.black
+    ..strokeCap = StrokeCap.round
+    ..strokeWidth = 10.0;
+
+  Signature({this.points});
+
+  @override
+  bool shouldRepaint(Signature oldDelegate) {
+    return oldDelegate.points != points;
+  }
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    for (int i = 0; i < points.length - 1; i++) {
+      if (points[i] != null && points[i + 1] != null) {
+        canvas.drawLine(points[i], points[i + 1], _paint);
+      }
+    }
+  }
+}
